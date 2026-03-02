73a17183408ac34e842065a7dac019f50498f296
Minitap <mini@minitap.ai>
2026-03-02 10:47:46 +0000

    fix(game): move scene presentation to viewDidLayoutSubviews
    - Move GameScene creation from viewDidLoad to viewDidLayoutSubviews
    - Ensures view bounds are properly set before creating scene
    - Add scenePresented flag to prevent multiple presentations
    - Fixes issue where scene was created with zero or incorrect bounds

--- FlappyDon/UI/GameViewController.swift
+++ FlappyDon/UI/GameViewController.swift
@@ -2,16 +2,13 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    
+    private var scenePresented = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Create GameScene with the view's bounds
-            let scene = GameScene(size: view.bounds.size)
-            scene.scaleMode = .aspectFill
-            view.presentScene(scene)
-            
             view.ignoresSiblingOrder = true
             view.showsFPS = true
             view.showsNodeCount = true
@@ -21,6 +18,18 @@ class GameViewController: UIViewController {
             #else
             view.preferredFramesPerSecond = 60
             #endif
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Present scene only once, after view bounds are set
+        if !scenePresented, let view = self.view as? SKView, view.bounds.size.width > 0 {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
+            scenePresented = true
         }
     }
 
